c1348cc2ce8140678923df165ee6e2fdea62c7de
AmadueszLew <lewandowskiamadeusz99@gmail.com>
2022-10-17 00:19:13 +0200

    snkrs-detail page css update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AuthorizationComponent} from './authorization/authorization.component'
 import { AuthorizationGuard } from './authorization/authorization.guard';
+import { SneakerDetailComponent } from './sneakers/sneaker-detail/sneaker-detail.component';
 import { SneakersComponent } from './sneakers/sneakers.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {path:'',component:SneakersComponent},
+  {path: ':id',component:SneakerDetailComponent},
   {path:'authorization', component:AuthorizationComponent},
   {path:'user',canActivate:[AuthorizationGuard],component:UserComponent}
   
